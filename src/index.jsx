83dfc1da5f05acf01d76ed78f4bd10cde6d39e62
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-05-22 10:00:31 +0200

    added mapbow package

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import mapboxgl from 'react-mapbox-gl';
 
 import '../assets/stylesheets/application.scss';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVkZW5hZ2FuIiwiYSI6ImNrNzZiOW92ZzBpbGMzZnIxZzUwNWdvcGsifQ.Q9bWDOQM5SXM5HOC2c-98Q';
 
 const Hello = ({ name }) => {
   return (
